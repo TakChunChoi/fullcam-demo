0784e4db4fc5e96d49dc942df9ea9c6d1978ae49
Tak Chun Choi <takchun.choi@gmail.com>
2021-10-06 11:56:21 +1100

    fix dependancy issue

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,13 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { SelectorComponent } from './selector/selector.component';
 import { InputFieldComponent } from './components/input-field/input-field.component';
 
 
 
 @NgModule({
   declarations: [
-    SelectorComponent,
     InputFieldComponent
   ],
   imports: [
